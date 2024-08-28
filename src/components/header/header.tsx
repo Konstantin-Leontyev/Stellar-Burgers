21769ab2f490e15f0d07fe9a42908d51f9c91592
Konstantin Leontyev <K.A.Leontyev@gmail.com>
2024-08-28 11:20:08 +0400

    Step-2 initial commit

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -11,12 +11,6 @@ export default function AppHeader() {
       <div className={`${styles.container} pb-4 pt-4`}>
         <nav>
           <ul className={`${styles.ul} ${styles.navItems}`}>
-            {/*
-            TODO
-            Пока не умею передавать компоненты в компонент.
-            Перенести теги ли в компонент HeaderNavItem, было плохой идеей им там не место.
-            Дале дале верну их обратно и оберну теги в цикл.
-          */}
             <HeaderNavItem>
               <BurgerIcon type="primary"/>
               <span className="text text_type_main-default ml-2">
@@ -34,13 +28,6 @@ export default function AppHeader() {
         <div className={styles.logo}>
           <Logo/>
         </div>
-        {/*
-        TODO
-        Далее мною добавлен еще один список.
-        Предполагается, у авторизованного пользователя
-        будет еще ссылка на страницу смены пароля и выйти.
-        Потому тут предполагается свой цикл.
-      */}
         <nav>
           <ul className={`${styles.ul} ${styles.authItems}`}>
             <HeaderNavItem>
